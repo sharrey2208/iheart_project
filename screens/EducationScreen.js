adc06047255b8e822b6072972910793b3263d978
SUHENDRA Sharrey <49983@2020-49983.lan>
2019-08-07 18:01:28 +0700

    interv

--- screens/EducationScreen.js
+++ screens/EducationScreen.js
@@ -68,7 +68,7 @@ function buttonPressed(props) {
 }
 
 function buttonP(props) {
-  props.navigation.navigate("pregnancy");
+  props.navigation.navigate("pr");
 }
 
 EducationScreen.navigationOptions = {
