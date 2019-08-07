b0365edce968a97d2d7658854c1531fe8dc083a0
SUHENDRA Sharrey <49983@2020-49983.lan>
2019-08-07 17:58:02 +0700

    interv

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -30,7 +30,7 @@ const HomeStack = createStackNavigator(
     interventions: DisabilityInterventionsScreen,
     ld: EducationLdScreen,
     therapy: EducationTherapiesScreen,
-    preg: EducationPlScreen
+    pregnancy: EducationPlScreen
   },
 
   config
